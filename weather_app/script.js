d1ebe5bdf67fcc3bd8f085e392e387f8a7b775ac
programmerNizam <mdnizam12109@gmail.com>
2024-01-01 23:11:49 +0600

    Weather api work and checkWeather function created

--- weather_app/script.js
+++ weather_app/script.js
@@ -1,2 +1,10 @@
-const apiKey = "appid=542120ab52b8d9c6b5b26680f38d0293";
+const apiKey = "542120ab52b8d9c6b5b26680f38d0293";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=dhaka";
+
+async function checkWeather() {
+    const response = await fetch(apiUrl + `&appid=${apiKey}`)
+    let data = await response.json();
+
+    console.log(data);
+}
+checkWeather()

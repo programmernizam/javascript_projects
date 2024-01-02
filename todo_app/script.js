c5082c282d53919dd0522fe74c877acc4cc1223d
programmerNizam <mdnizam12109@gmail.com>
2024-01-02 18:06:41 +0600

    Todo add task function working

---
+++ todo_app/script.js
@@ -0,0 +1,13 @@
+const inputBox = document.getElementById("input_box")
+const listContainer = document.getElementById("list_container")
+
+function addTask() {
+    if (inputBox.value === '') {
+        alert("You must write a task")
+    }
+    else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+    }
+}

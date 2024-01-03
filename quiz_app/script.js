a8a836ce5159ed67c18155478ab0255471308a95
programmerNizam <mdnizam12109@gmail.com>
2024-01-03 18:00:24 +0600

    All Questions and aswers added

---
+++ quiz_app/script.js
@@ -0,0 +1,38 @@
+const questions = [
+    {
+        question: "Which is the largest animal in the world?",
+        answers: [
+            { text: "Shark", correct: false },
+            { text: "Blue Whale", correct: true },
+            { text: "Giraffe", correct: false },
+            { text: "Elephant", correct: false }
+        ]
+    },
+    {
+        question: "Which is the smallest continent in the world?",
+        answers: [
+            { text: "Asia", correct: false },
+            { text: "Australia", correct: true },
+            { text: "Arctic", correct: false },
+            { text: "Africa", correct: false }
+        ]
+    },
+    {
+        question: "What is the capital city of France?",
+        answers: [
+            { text: "Berlin", correct: false },
+            { text: "Madrid", correct: false },
+            { text: "Rome", correct: false },
+            { text: "Paris", correct: true }
+        ]
+    },
+    {
+        question: "Which element is known as the 'King of Chemicals'?",
+        answers: [
+            { text: "Hydrogen", correct: false },
+            { text: "Oxygen", correct: false },
+            { text: "Sulfuric Acid", correct: true },
+            { text: "Nitrogen", correct: false }
+        ]
+    }
+]

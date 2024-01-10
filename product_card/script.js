5881df949215cbb360df7695a984f2ec10993e82
programmerNizam <mdnizam12109@gmail.com>
2024-01-10 19:28:53 +0600

    Product card initial value set on javascript

--- product_card/script.js
+++ product_card/script.js
@@ -5,3 +5,7 @@ const button = document.getElementById("addToCart")
 // States or data
 const productPrice = 5000;
 let totalPrice = 0;
+
+// Set product Initially
+price.innerText = `৳ ${productPrice}`
+total.innerText = `Total: ৳ ${totalPrice}`

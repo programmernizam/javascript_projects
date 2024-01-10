a20025d86c383f24bb7cf4c018dba8f1df712232
programmerNizam <mdnizam12109@gmail.com>
2024-01-10 19:26:53 +0600

    Product card states and element called in javascript

---
+++ product_card/script.js
@@ -0,0 +1,7 @@
+const price = document.getElementById("price")
+const total = document.getElementById("total")
+const button = document.getElementById("addToCart")
+
+// States or data
+const productPrice = 5000;
+let totalPrice = 0;
